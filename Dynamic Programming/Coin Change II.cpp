e6e7579819d02ad68493e48796b30cab30ae1462
Junaid Bin Jahangir <164465375+junaidr04@users.noreply.github.com>
2025-11-30 11:23:18 +0600

    Add files via upload

---
+++ Dynamic Programming/Coin Change II.cpp
@@ -0,0 +1,17 @@
+class Solution
+{
+public:
+    int change(int amount, vector<int> &coins)
+    {
+        vector<unsigned long long> dp(amount + 1, 0);
+        dp[0] = 1;
+        for (int c : coins)
+        {
+            for (int i = c; i <= amount; i++)
+            {
+                dp[i] += dp[i - c];
+            }
+        }
+        return dp[amount];
+    }
+};
