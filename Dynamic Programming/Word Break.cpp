8aa3b4509a03523f0385a02cfa86b1e4aedd680c
Junaid Bin Jahangir <164465375+junaidr04@users.noreply.github.com>
2025-12-04 12:00:58 +0600

    Add files via upload

---
+++ Dynamic Programming/Word Break.cpp
@@ -0,0 +1,23 @@
+class Solution
+{
+public:
+    bool wordBreak(string s, vector<string> &wordDict)
+    {
+        int n = s.size();
+        vector<int> dp(n + 1, false);
+        dp[0] = true;
+        for (int i = 1; i <= n; i++)
+        {
+            for (string &w : wordDict)
+            {
+                int start = i - w.length();
+                if (start >= 0 && dp[start] && s.substr(start, w.length()) == w)
+                {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[n];
+    }
+};
