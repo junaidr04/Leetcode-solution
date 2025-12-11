32234f83922ca115b02e4f3fa179504aeacbac2b
Junaid Bin Jahangir <164465375+junaidr04@users.noreply.github.com>
2025-12-11 09:22:43 +0600

    Add files via upload

---
+++ Greedy Algorithm/Non-overlapping Intervals.cpp
@@ -0,0 +1,29 @@
+class Solution
+{
+public:
+    int eraseOverlapIntervals(vector<vector<int>> &intervals)
+    {
+        if (intervals.empty())
+            return 0;
+        sort(intervals.begin(), intervals.end(),
+             [](const vector<int> &a, const vector<int> &b)
+             {
+                 return a[1] < b[1];
+             });
+        int count = 0;
+        int end = intervals[0][1];
+        for (int i = 1; i < intervals.size(); i++)
+        {
+            int start = intervals[i][0], End = intervals[i][1];
+            if (start < end)
+            {
+                count++;
+            }
+            else
+            {
+                end = End;
+            }
+        }
+        return count;
+    }
+};
