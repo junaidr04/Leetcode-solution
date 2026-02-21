628d800206763f7808ee656eeae56b72b083bc00
Junaid Bin Jahangir <164465375+junaidr04@users.noreply.github.com>
2026-02-21 12:43:36 +0600

    Add files via upload

---
+++ Two pointer/3Sum Closest.cpp
@@ -0,0 +1,41 @@
+// Problem: 3Sum Closest
+// Platform: leetcode
+// Link: https://leetcode.com/problems/3sum-closest/
+// time complexity: O(n^2)
+// space complexity: O(1)
+
+class Solution
+{
+public:
+    int threeSumClosest(vector<int> &nums, int target)
+    {
+        sort(nums.begin(), nums.end());
+        int bestSum = nums[0] + nums[1] + nums[2];
+        int n = nums.size();
+        for (int i = 0; i < n - 2; i++)
+        {
+            int l = i + 1, r = n - 1;
+            while (l < r)
+            {
+                int sum = nums[i] + nums[l] + nums[r];
+                if (abs(target - sum) < abs(target - bestSum))
+                {
+                    bestSum = sum;
+                }
+                else if (abs(target - sum) == abs(target - bestSum))
+                {
+                    bestSum = max(bestSum, sum);
+                }
+                if (sum < target)
+                {
+                    l++;
+                }
+                else
+                {
+                    r--;
+                }
+            }
+        }
+        return bestSum;
+    }
+};
